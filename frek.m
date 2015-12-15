56597f954a4137c14daaa1fd0e9615b7ce1830b3
onurtemizgezek <onurtemizgezek@gmail.com>
2015-12-15 13:35:22 +0200

    notaların frekans karşılıklarını getiren fonksiyon.

---
+++ frek.m
@@ -0,0 +1,17 @@
+function ff=frek(nota,oktav)
+notalar=['A' 'a' 'B' 'b'];
+referans=16.35;
+p=length(notalar);
+
+for okt=0:8
+    if okt==oktav
+        
+for j=1:p
+    if nota==char(notalar(j)) 
+        ff=2^okt*(2^((j-1)/12)*referans);
+    end
+end
+
+    end
+end
+end
